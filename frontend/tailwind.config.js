8d7cdb1b383c48297fbd9e39b5ddebac2ade5b5d
Nader-barhoumi <naderbarhoumi.01@outlook.fr>
2025-04-04 23:59:12 +0100

    Initial commit: Fullstack app with Angular + Express + PostgreSQL + Redis

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,scss}"],
+  theme: { extend: {} },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
+
