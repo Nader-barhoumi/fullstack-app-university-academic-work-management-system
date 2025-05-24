b7e02d5e0d9424ad871658fd1669b81f53612c58
Nader-barhoumi <naderbarhoumi.01@outlook.fr>
2025-05-24 05:00:20 +0100

    feat: Remove Academic Institution module and related components, update routing to use AddressesListComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AcademicInstitutionModule } from './academic-institution/academic-institution.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AcademicInstitutionModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 
 import { AddressesListComponent } from './addresses/address-list/addresses-list.component';
 import { AddressFormComponent } from './addresses/address-form/address-form.component';
-import { AcademicInstitutionComponent } from './academic-institution/academic-institution.component';
 
 export const routes: Routes = [
   { path: 'addresses', component: AddressesListComponent },
   { path: 'addresses/create', component: AddressFormComponent },
   { path: 'addresses/edit/:id', component: AddressFormComponent },
   {path: '', redirectTo: 'uni', pathMatch: 'full'},
-  {path: 'uni', component: AcademicInstitutionComponent }
+  {path: 'uni', component: AddressesListComponent}, 
 ];
